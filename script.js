bb583f5750bf59bc7a6ea18198d7264ab2bd8ebe
SreenathSreekrishna <sreenathsreekrishna21@gmail.com>
2023-08-15 16:21:20 +0530

    added themes

--- script.js
+++ script.js
@@ -9,3 +9,27 @@ for (let i of linkElements) {
         window.location.href = links[linkName];
     }
 }
+
+const theme = document.querySelector('.theme');
+const themes = {
+    light: ['#112d38', '#ffffff', '#9bc3dd'],
+    dark: ['#9bc3dd', '#ffffff', '#112d38']
+}
+const themeChange = () => {
+    const theme = window.localStorage.getItem('theme');
+    const newTheme = theme ? (theme === 'dark' ? 'light' : 'dark') : 'light';
+    window.localStorage.setItem('theme', newTheme);
+    if (newTheme === 'light') {
+        document.documentElement.style.setProperty('--primary-color', themes.light[0]);
+        document.documentElement.style.setProperty('--text-color', themes.light[1]);
+        document.documentElement.style.setProperty('--bg-color', themes.light[2]);
+    }
+    else {
+        document.documentElement.style.setProperty('--primary-color', themes.dark[0]);
+        document.documentElement.style.setProperty('--text-color', themes.dark[1]);
+        document.documentElement.style.setProperty('--bg-color', themes.dark[2]);
+    }
+}
+theme.onclick = themeChange;
+themeChange();
+themeChange();
