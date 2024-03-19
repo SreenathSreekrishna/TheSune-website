9a69079d4041d5387ec0225cebb82b1e521370f1
SreenathSreekrishna <sreenathsreekrishna21@gmail.com>
2024-03-19 21:46:40 +0530

    ^3athon

--- src/pages/^3athon/^3athon.jsx
+++ src/pages/^3athon/^3athon.jsx
@@ -1,5 +1,54 @@
-const Sune10 = () => {
-    return <div>^3athon</div>;
+import './^3athon.css';
+
+const _3athon = () => {
+    return <div>
+        <div className='heading heading-^3athon'>
+            ^3athon
+        </div>
+        <div className='^3athon'>
+            <div className='text'>The first inter-school 3x3 and 2x2 cubing competition of TheSune was organized in collaboration with the school tech club – Code Warriors. The event was held in two parts : The Online Prelims and The Offline Finals. This event was a huge success and saw participation of most major schools of Delhi.</div>
+            <div className='results'>
+                <div className='event 2x2'>
+                    <div className='event-heading'>
+                        ^3athon 2 x 2
+                    </div>
+                    <div className='event-data'>
+                        <div className='result-data'>
+                            1. Garv Jhuremalani 
+                        <span className="school">(Delhi Public School, R.K. Puram)</span>
+                        </div>
+                        <div className='result-data'>
+                            2. Rehan Mongia
+                        <span className="school">(Airforce Bal Bharti School)</span>
+                        </div>
+                        <div className='result-data'>
+                            3. Pranav Chandra
+                        <span className="school">(Delhi Public School, Mathura Road)</span>
+                        </div>
+                    </div>
+                </div>
+                <div className='event 3x3'>
+                    <div className='event-heading'>
+                        ^3athon 3 x 3
+                    </div>
+                    <div className='event-data'>
+                        <div className='result-data'>
+                            1. Shreyas Anand 
+                        <span className="school">(Air Force Bal Bharati School) </span>
+                        </div>
+                        <div className='result-data'>
+                            2. Nikhil Lal 
+                        <span className="school">(Delhi Public School, R.K. Puram)</span>
+                        </div>
+                        <div className='result-data'>
+                            3. Arush Kashyap 
+                        <span className="school">(Amity International School Gurugram Sector 46)</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>;
 }
 
-export default Sune10;
+export default _3athon;
