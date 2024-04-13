04df41b5cf5a48d9e34ef6e78b3f1f610ee8ffe1
SreenathSreekrishna <sreenathsreekrishna21@gmail.com>
2024-04-13 16:31:29 +0530

    added 2023 cubathon, made archive

--- src/pages/^3athon/^3athon.jsx
+++ src/pages/^3athon/^3athon.jsx
@@ -6,6 +6,7 @@ const _3athon = () => {
             ^3athon
         </div>
         <div className='^3athon'>
+            <div className='minor-heading'>2022</div>
             <div className='text'>The first inter-school 3x3 and 2x2 cubing competition of TheSune was organized in collaboration with the school tech club – Code Warriors. The event was held in two parts : The Online Prelims and The Offline Finals. This event was a huge success and saw participation of most major schools of Delhi.</div>
             <div className='results'>
                 <div className='event 2x2'>
@@ -43,6 +44,67 @@ const _3athon = () => {
                         <div className='result-data'>
                             3. Arush Kashyap 
                         <span className="school">(Amity International School Gurugram Sector 46)</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className='minor-heading'>2023</div>
+            <div className='text'>The second inter-school 3x3 and 2x2 cubing competition of The Sune was organized in collaboration with the school's technology club – Code Warriors. The event was held in two phases, The Online Prelims and The Offline Finals. The event consisted of 3 categories namely, 2x2, 3x3 and x³. The event was a massive success and saw participation of various prominent schools of Delhi. The results of the event were as follows:</div>
+            <div className='results'>
+                <div className='event 2x2'>
+                    <div className='event-heading'>
+                        ^3athon 2 x 2
+                    </div>
+                    <div className='event-data'>
+                        <div className='result-data'>
+                            1. Garv Jhuremalani 
+                        <span className="school">(Delhi Public School, R.K. Puram)</span>
+                        </div>
+                        <div className='result-data'>
+                            2. Pranav Chandra
+                        <span className="school">(Delhi Public School, Dwarka)</span>
+                        </div>
+                        <div className='result-data'>
+                            3. Arnav Bansal 
+                        <span className="school">(Delhi Public School, Noida)</span>
+                        </div>
+                    </div>
+                </div>
+                <div className='event 3x3'>
+                    <div className='event-heading'>
+                        ^3athon 3 x 3
+                    </div>
+                    <div className='event-data'>
+                        <div className='result-data'>
+                            1. Ronit Arora 
+                        <span className="school">(Rukmini Devi Public School)</span>
+                        </div>
+                        <div className='result-data'>
+                            2. Nikhil Lal 
+                        <span className="school">(Delhi Public School, R.K. Puram)</span>
+                        </div>
+                        <div className='result-data'>
+                            3. Rehan Mongia
+                        <span className="school">(Air Force Bal Bharti School)</span>
+                        </div>
+                    </div>
+                </div>
+                <div className='event x³'>
+                    <div className='event-heading'>
+                        ^3athon x³ (team event)
+                    </div>
+                    <div className='event-data'>
+                        <div className='result-data'>
+                            1. Nikhil Lal and Garv Jhuremalani  
+                        <span className="school">(Delhi Public School, R.K. Puram)</span>
+                        </div>
+                        <div className='result-data'>
+                            2. Prisha Bhatia and Arush Kashyap  
+                        <span className="school">(Amity International Sector 46)</span>
+                        </div>
+                        <div className='result-data'>
+                            3. Rehan Mongia and Nakshastra Aggarwal
+                        <span className="school">(Air Force Bal Bharti School)</span>
                         </div>
                     </div>
                 </div>
